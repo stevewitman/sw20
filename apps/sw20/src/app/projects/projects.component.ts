7571f2f541e5aaf8e7019bc3f4663a4c69c630c7
sw <sw@sws-MacBook-Pro.local>
2020-03-14 21:39:28 -0600

    Added form and crud methods to projects component

--- apps/sw20/src/app/projects/projects.component.ts
+++ apps/sw20/src/app/projects/projects.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Project, ProjectsService } from '@bb/core-data';
+import { ProjectsFacade } from '@bb/core-state';
+
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'bb-projects',
@@ -6,10 +12,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+  projects$: Observable<Project[]> = this.projectsFacade.allProjects$
+  project: Project;
+  formGroup: FormGroup;
 
-  constructor() { }
+  constructor(
+    private projectsService: ProjectsService,
+    private formBuilder: FormBuilder,
+    private projectsFacade: ProjectsFacade
+  ) { }
 
   ngOnInit(): void {
+    this.initForm();
+    this.projectsFacade.loadProjects();
+    this.projectsFacade.nutations$.subscribe(() => this.reset())
+  }
+
+  reset() {
+    this.formGroup.reset();
+    this.project = {} as Project;
+    Object.keys(this.formGroup.controls).forEach(key => {
+      this.formGroup.get(key).setErrors(null);
+    })
+  }
+
+  select(project: Project) {
+    this.projectsFacade.selectProject(project.id);
+    this.formGroup.patchValue(project);
+  }
+
+  create() {
+    this.projectsFacade.createProject(this.formGroup.value);
+  }
+
+  update() {
+    this.projectsFacade.updateProject(this.formGroup.value);
+  }
+
+  delete(project: Project) {
+    this.projectsFacade.deleteProject(project);
+  }
+
+  save(project: Project) {
+    if (project.id) {
+      this.update();
+    } else {
+      this.create();
+    }
+  }
+
+  initForm() {
+    this.formGroup = this.formBuilder.group({
+      id:[null],
+      title: ['', Validators.compose([
+        Validators.required
+      ])],
+      details: ['', Validators.compose([
+        Validators.required
+      ])],
+      importanceLevel: [0]
+    })
   }
 
 }
